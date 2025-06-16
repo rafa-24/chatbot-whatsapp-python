58c75f1813f37b941db0bde86b03bb2d27cab9b0
rafa-24 <rafaprieto063@gmail.com>
2025-06-16 14:03:08 -0500

    changes

--- FlaskChatBot/app.py
+++ FlaskChatBot/app.py
@@ -1,24 +1,37 @@
-from flask import Flask, request
-import sett
-
-# create instace of Flask
+#ChatBot inteligente con WhatsApp en Python
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+#CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    #SI HAY DATOS RECIBIDOS VIA GET
+    if request.method == "GET":
+        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if request.args.get('hub.verify_token') == "chatbotpython":
+            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return request.args.get('hub.challenge')
+        else:
+            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+          return "Error de autentificacion."
+    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    data=request.get_json()
+    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
+    telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+    #EXTRAEMOS EL TELEFONO DEL CLIENTE
+    mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
+    idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
+    #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
+    timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
+    #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+    #SI HAY UN MENSAJE
+    if mensaje is not None:
+      f = open("texto.txt", "w")
+      f.write(mensaje)
+      f.close()
+      #RETORNAMOS EL STATUS EN UN JSON
+      return jsonify({"status": "success"}, 200)
 
-@app.route('/')
-def index():
-  return 'Hello, World!'
-
-
-@app.route('/webhook', methods=['GET', 'POST'])
-def verify_token():
-  try:
-    if request.method == 'GET':
-      if request.args.get('hub.verify_token') == sett.token: # validate token
-        return request.args.get('hub.challenge'), 200
-      return 'Invalid token', 403
-    elif request.method == 'POST':
-      data = request.get_json()
-      print(data)
-      return 'Event received', 200
-  except Exception as e:
-    return e,403
+#INICIAMSO FLASK
+if __name__ == "__main__":
+  app.run(debug=True)
